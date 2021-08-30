fa5a5a6154992fcef40cbe4a27f5f3fb465cdee5
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-08-30 22:59:25 +0100

    Update
    Error component and commentarea skeleton

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CommentArea extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
